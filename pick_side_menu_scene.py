efc5f227faaf59f433c269c4bb8ea31523ec21cb
Sanzhar <sanzhar.maratov@proton.me>
2024-03-01 12:30:47 +0600

    pick side menu scene

---
+++ pick_side_menu_scene.py
@@ -0,0 +1,71 @@
+from engine.engine import Engine
+from engine.asset_loading import load_assets_from_config
+from engine.input_actions_loading import load_input_actions_from_file
+from engine.transform import Transform
+
+from engine.render_system import Renderer
+from engine.input_system import InputSystem
+from menu_control_system import MenuControlSystem
+from menu_scene_switch_system import MenuSceneSwitchSystem
+from create_menu_entities import create_cursor, create_menu_button,\
+        create_background
+import pygame as pg
+
+def pick_side_menu_scene(engine: Engine) -> None:
+    actions, action_to_kbd, action_to_mouse, _ = \
+            load_input_actions_from_file("config/input_actions.json")
+
+    start_scene = engine.add_scene("PICK_SIDE")
+
+    context = engine.context()
+    em = start_scene.entity_manager()
+    assets = load_assets_from_config("config/assets.json")
+
+    create_background(
+            em,
+            assets,
+            "IMAGE_PICK_SIDE_MENU_BACKGROUND"
+            )
+    cursor = create_cursor(em, pg.Vector2(2, 4))
+
+    create_menu_button(
+            em,
+            "GAMEPLAY_WHITE",
+            pg.Vector2(2, 4),
+            pg.Vector2(1, 4)
+            )
+    create_menu_button(
+            em,
+            "GAMEPLAY_BLACK",
+            pg.Vector2(2, 5),
+            pg.Vector2(1, 3)
+            )
+    create_menu_button(
+            em,
+            "START",
+            pg.Vector2(2, 6),
+            pg.Vector2(1, 3)
+            )
+    render_sys = Renderer(
+            context.camera(), 
+            context.window().surface(),
+            context.window().tile_size()
+            )
+
+    cursor_transform = cursor.get_component(Transform)
+    if not cursor_transform:
+        context.set_scene_to_switch("EXIT")
+        context.switch_to_next()
+        return
+
+    input_system = InputSystem(actions, action_to_kbd, action_to_mouse)
+    control_system = MenuControlSystem(start_scene.context(), 6, 4)
+    menu_scene_switch_system = MenuSceneSwitchSystem(
+            cursor_transform,
+            context
+            )
+
+    start_scene.add_system(input_system)
+    start_scene.add_system(render_sys)
+    start_scene.add_system(control_system)
+    start_scene.add_system(menu_scene_switch_system)
